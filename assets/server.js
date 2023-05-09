b5796353b9ad3002e1e6842689feea906730ae6d
Ethan Daniel <e.daniel.small@gmail.com>
2023-05-09 13:12:16 -0700

    creating main menu

---
+++ assets/server.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// main menu with options
+
+function mainMenu() {
+    console.log("you have reached the main menu!")
+};
+
+mainMenu();
