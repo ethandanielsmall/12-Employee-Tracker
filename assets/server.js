cdcb29e5d44844866ee8ffca7c1e47d7b49094a3
Ethan Daniel <e.daniel.small@gmail.com>
2023-05-09 13:35:51 -0700

    working on employee table

--- assets/server.js
+++ assets/server.js
@@ -59,3 +59,36 @@ function mainMenu() {
       };
 
 mainMenu();
+
+// ------------------------------------------ VIEW EMPLOYEES
+function viewEmployees() {
+    var query =
+    `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  LEFT JOIN employee m
+	ON m.id = e.manager_id`
+
+
+// employee ids, first names, last names, job titles, departments, salaries,
+// and managers that the employees report to
+}
+
+// ------------------------------------------ ADD EMPLOYEES
+
+// ------------------------------------------ REMOVE EMPLOYEE
+
+// ------------------------------------------ VIEW ROLES
+
+// ------------------------------------------ UPDATE EMPLOYEE ROLE
+
+// ------------------------------------------ ADD ROLE
+
+// ------------------------------------------ VIEW DEPARTMENTS
+
+// ------------------------------------------ ADD DEPARTMENT
+
+// ------------------------------------------ REMOVE DEPARTMENT
