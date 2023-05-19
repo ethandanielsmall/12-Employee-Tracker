dce227f3b7426db4b43cb41946f8452a0be7df02
Ethan Daniel <e.daniel.small@gmail.com>
2023-05-19 16:02:20 -0700

    added README desc

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-##12 employee tracker
+# 12 Employee Tracker
+  
+  ## Description
+  
+  This project is a server that keeps track of various things a company may need to keep track of such as employees, employee status, employee rols, employee departments, etc.
--- assets/server.js
+++ assets/server.js
@@ -1,7 +1,12 @@
 // const mysql = require("mysql");
 const inquirer = require("inquirer");
 
-// connection needed?
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  database: "employeeTrackerDB"
+});
 
 // main menu with options
 
